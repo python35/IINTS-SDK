da07333b35020999636fa7e1a9605da1e2daea91
Rune <rune.bobbaers@gmail.com>
2026-06-12 23:12:48 +0200

    docs: enable MathJax for LaTeX formulas

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,18 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.document.state(0);
+  MathJax.texReset();
+  MathJax.typeset();
+});
